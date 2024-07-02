5f8a203f23b95e3268ba4264ec71c15f3d2ab69e
Antonio Sabino <aslimajr@outlook.com>
2024-07-02 20:52:30 -0300

    testando ProductAdmFacade

---
+++ src/modules/product-adm/facade/product-adm.facade.spec.ts
@@ -0,0 +1,53 @@
+import { Sequelize } from 'sequelize-typescript'
+import { ProductModel } from '../repository/product.model'
+import ProductRepository from '../repository/product.repository'
+import AddProductUsecase from '../usecase/add-product/add-product.usecase'
+import ProductAdmFacade from './product-adm.facade'
+
+describe('ProductAdmFacade', () => {
+	let sequelize: Sequelize
+
+	beforeAll(async () => {
+		sequelize = new Sequelize({
+			dialect: 'sqlite',
+			storage: ':memory:',
+			logging: false,
+			sync: { force: true },
+		})
+
+		sequelize.addModels([ProductModel])
+		await sequelize.sync()
+	})
+
+	afterAll(async () => {
+		await sequelize.close()
+	})
+
+	it('should create a product', async () => {
+		const productRepository = new ProductRepository()
+		const addProductUseCase = new AddProductUsecase(productRepository)
+		const productAdmFacade = new ProductAdmFacade({
+			addUseCase: addProductUseCase,
+			stockUseCase: undefined,
+		})
+
+		const input = {
+			id: '1',
+			name: 'Product 1',
+			description: 'Product 1 description',
+			purchasePrice: 10,
+			stock: 10,
+		}
+
+		await productAdmFacade.addProduct(input)
+
+		const product = await ProductModel.findByPk('1')
+
+		expect(product).toBeDefined()
+		expect(product.id).toBe('1')
+		expect(product.name).toBe('Product 1')
+		expect(product.description).toBe('Product 1 description')
+		expect(product.purchasePrice).toBe(10)
+		expect(product.stock).toBe(10)
+	})
+})
