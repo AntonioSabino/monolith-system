7ffaaf40ecc592aeadf5706f9ff0cf0c7137f0b5
Antonio Sabino <aslimajr@outlook.com>
2024-07-02 20:57:15 -0300

    implementando ProductAdmFacadeFactory

--- src/modules/product-adm/facade/product-adm.facade.spec.ts
+++ src/modules/product-adm/facade/product-adm.facade.spec.ts
@@ -1,8 +1,6 @@
 import { Sequelize } from 'sequelize-typescript'
 import { ProductModel } from '../repository/product.model'
-import ProductRepository from '../repository/product.repository'
-import AddProductUsecase from '../usecase/add-product/add-product.usecase'
-import ProductAdmFacade from './product-adm.facade'
+import ProductAdmFacadeFactory from '../factory/facade.factory'
 
 describe('ProductAdmFacade', () => {
 	let sequelize: Sequelize
@@ -24,12 +22,7 @@ describe('ProductAdmFacade', () => {
 	})
 
 	it('should create a product', async () => {
-		const productRepository = new ProductRepository()
-		const addProductUseCase = new AddProductUsecase(productRepository)
-		const productAdmFacade = new ProductAdmFacade({
-			addUseCase: addProductUseCase,
-			stockUseCase: undefined,
-		})
+		const productAdmFacade = ProductAdmFacadeFactory.create()
 
 		const input = {
 			id: '1',
