61d7d9e586c25c9805437ef88ec80bc1cf999ff6
Antonio Sabino <aslimajr@outlook.com>
2025-05-27 17:11:31 -0300

    criando teste de getProduct

--- src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
+++ src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
@@ -1,6 +1,8 @@
 import { PlaceOrderInputDto } from './place-order.dto'
 import PlaceOrderUseCase from './place-order.usecase'
 
+const mockDate = new Date(2000, 1, 1)
+
 describe('PlaceOrderUseCase', () => {
 	describe('validateProducts method', () => {
 		// @ts-expect-error - no params in constructor
@@ -59,6 +61,33 @@ describe('PlaceOrderUseCase', () => {
 			expect(mockProductFacade.checkStock).toHaveBeenCalledTimes(5)
 		})
 	})
+
+	describe('getProducts method', () => {
+		beforeAll(() => {
+			jest.useFakeTimers('modern')
+			jest.setSystemTime(mockDate)
+		})
+
+		afterAll(() => {
+			jest.useRealTimers()
+		})
+
+		// @ts-expect-error - no params in constructor
+		const placeOrderUseCase = new PlaceOrderUseCase()
+
+		it('should throw an error when no product not found', async () => {
+			const mockCatalogFacade = {
+				find: jest.fn().mockResolvedValue(null),
+			}
+			// @ts-expect-error - force set catalogFacade
+			placeOrderUseCase['_catalogFacade'] = mockCatalogFacade
+
+			await expect(placeOrderUseCase['getProduct']('0')).rejects.toThrow(
+				new Error('Product not found')
+			)
+		})
+	})
+
 	describe('execute method', () => {
 		it('should throw an error when client not found', async () => {
 			const mockClientFacade = {
